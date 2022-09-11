70c5973e7e1dc9b5ea98a9475c3ddebc72b9c64f
Hazal Gunduz <87630881+Gunduzhazal@users.noreply.github.com>
2022-09-11 17:47:45 -0400

    Add files via upload

---
+++ Finall projectt proposall.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### DATA 620 - FINAL PROJECT PROPOSAL
+
+# Hazal Gunduz
+
+# WINTER IS COMING!
+# A Game of Thrones is the first novel in A Song of Ice and Fire, a series of fantasy novels by American author George R. R. Martin. There's currently five! main Game of Thrones books out right now, and two more are on the way. Plus, there's a prequel book, which is what House of Dragon is based on.
+# I will analyze the network of characters in the GAME OF THRONES books. Here, two characters are considered to have appeared together if their names appear around 15 words from each other in the books.
